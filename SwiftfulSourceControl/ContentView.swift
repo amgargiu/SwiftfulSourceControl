c3eaeffb7e36bff480c4ba9448d377b5da27dfc6
Antonio <antonio.m.gargiulo@gmail.com>
2025-11-13 19:39:30 -0500

    Added save button

--- SwiftfulSourceControl/ContentView.swift
+++ SwiftfulSourceControl/ContentView.swift
@@ -15,6 +15,9 @@ struct ContentView: View {
                 .foregroundStyle(.tint)
             Text("Swiftful")
 
+            Button("save") {
+                
+            }
         }
         .padding()
     }
